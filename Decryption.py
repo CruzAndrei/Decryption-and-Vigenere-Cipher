acbbb2beeb86f3ed2661e051fa9dd873ce78d72f
CruzAndrei <cruzandrei237@gmail.com>
2023-05-03 09:50:19 +0800

    Psuedocode

---
+++ Decryption.py
@@ -0,0 +1,11 @@
+#Welcoming the user
+#Import of modules
+#Asking user for inputs to their data
+#Table of values
+#Inspection of each character
+#Substition for * is working
+#Subtitution for & is working
+#Subtitution for # is working
+#Subtitution for + is working
+#Subtitution for ! is working
+#Print output is working
