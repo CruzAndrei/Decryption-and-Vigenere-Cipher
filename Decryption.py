34f9cf8892c0b71f6de97e6244d4aee81c7c73d9
CruzAndrei <cruzandrei237@gmail.com>
2023-05-03 09:55:48 +0800

    Introducing the Program

--- Decryption.py
+++ Decryption.py
@@ -1,4 +1,7 @@
 #Welcoming the user
+print ("=" * 115)
+print ("\n\t\tHello there and welcome! This is a program to decode specific symbols into a character.\n\t\t\t\t\tCome and help me decrypt this texts\n")
+print ("=" * 115)
 #Import of modules
 #Asking user for inputs to their data
 #Table of values
