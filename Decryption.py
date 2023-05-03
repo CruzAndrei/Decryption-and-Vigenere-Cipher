178bb4f962ae82e968bb7590d362afb9d69489ab
CruzAndrei <cruzandrei237@gmail.com>
2023-05-03 09:58:18 +0800

    Adding modules and continuation to the decryption

--- Decryption.py
+++ Decryption.py
@@ -3,6 +3,12 @@ print ("=" * 115)
 print ("\n\t\tHello there and welcome! This is a program to decode specific symbols into a character.\n\t\t\t\t\tCome and help me decrypt this texts\n")
 print ("=" * 115)
 #Import of modules
+import colorama
+import pyfiglet
+from colorama import Fore
+colorama.init(autoreset=True)
+enter_cont = input('\n\t\t\t\t\t  "Enter to Continue"\n')
+print ("=" * 115)
 #Asking user for inputs to their data
 #Table of values
 #Inspection of each character
