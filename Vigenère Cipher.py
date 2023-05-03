2097d3cbece2a4f6df158815be62471c6ab9dba8
CruzAndrei <cruzandrei237@gmail.com>
2023-05-03 10:17:41 +0800

    Psuedocode

---
+++ Vigenère Cipher.py
@@ -0,0 +1,11 @@
+#Import of modules
+#Welcoming the user
+#Alphabet characters, both uppercase and lowercase
+#Defining the encryption
+#Inspection in message and key
+#Conjoining message and key input to cipher the text
+#Defining decrytion
+#Inspection in key and ciphered text
+#Decrypting the ciphered text
+#message input and key input
+#print of message, and key input and, the ciphered text and decrypted text
