0f8f8b0296232eb5dc84ac2d5fb5fc5c1b32bea1
CruzAndrei <cruzandrei237@gmail.com>
2023-05-03 10:30:23 +0800

    Introducing the Program

--- Vigenère Cipher.py
+++ Vigenère Cipher.py
@@ -6,6 +6,11 @@ import colorama
 from colorama import Fore
 colorama.init(autoreset=True)
 #Welcoming the user
+print ("-" * 115)
+print ("\n\t\tHello there and welcome to a program that uses Vigenère Cipher to Ciphertext a text!!\n\t\t\tCome and help me decrypt this texts and find out its true meaning!!\n")
+print ("-" * 115)
+enter_cont = input('\n\t\t\t\t\t\t"Enter to Continue"\n')
+print ("-" * 115)
 #Alphabet characters, both uppercase and lowercase
 #Defining the encryption
 #Inspection in message and key
